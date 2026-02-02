3446c0c4276b88c9f74f589fb75222164a7483fb
Mitalisingh4 <112572051+Mitalisingh4@users.noreply.github.com>
2026-02-02 21:28:58 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -20,6 +20,7 @@
 		   
 		  </div>
 		  <div class="mb-3">
+			  
 		    <label for="exampleInputPassword1" class="form-label">Email:</label>
 		    <input type="password" class="form-control" id="exampleInputPassword1">
 		  </div>
@@ -39,6 +40,7 @@
 		  <button type="submit" class="btn btn-primary mt-4">Submit</button>
 		</form>
     </div>
+	  jhbvv
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
